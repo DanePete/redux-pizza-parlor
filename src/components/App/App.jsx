1453fa222ab696bc7b5c50945bcde1eeb05f822d
dane <dane.m.petersen@gmail.com>
2021-08-19 11:55:05 -0500

    created axios get request as well as created order screen component. Imported order screen component into app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import OrderScreen from '../OrderScreen/OrderScreen';
+import { useEffect } from 'react';
 
 function App() {
+
+  let [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetchPizzas();
+  }, [])
+
+  const fetchPizzas = () => {
+    axios({
+      method: 'GET',
+      url: '/api/pizza'
+    }).then((response) => {
+      console.log('our response data', response.data);
+      setPizzas(response.data);
+    }).catch(err => {
+      console.log('ERROR ON GET /pizzas', err);
+    })
+  }
 
   return (
     <div className='App'>
@@ -12,7 +32,9 @@ function App() {
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+      <OrderScreen 
+        list = {pizzas}
+      />
     </div>
   );
 }
